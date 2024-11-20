04a78498e8b7cb6dba81e354c7b62f82549959f6
Tushar Debnath <endersmoon@gmail.com>
2024-11-20 15:55:50 +0530

    changes

--- app/puck/[...puckPath]/client.tsx
+++ app/puck/[...puckPath]/client.tsx
@@ -3,10 +3,30 @@
 import type { Data } from "@measured/puck";
 import { Puck } from "@measured/puck";
 import config from "../../../puck.config";
+import { PyramidIcon } from "lucide-react";
+
+
 
 export function Client({ path, data }: { path: string; data: Partial<Data> }) {
   return (
     <Puck
+    overrides={{
+        header: ({ actions }) => (
+          <div className='flex items-center justify-between w-screen p-3 border-b'>
+            <div className='flex items-center gap-2'>
+              <div className='flex items-center justify-center bg-gray-100 rounded-full size-11'> 
+                <PyramidIcon size={24
+                  
+                } />
+              </div> 
+            <div className='text-xl font-medium '>PRISM</div>
+            </div>
+            
+            <div>{actions}</div>
+          </div>
+        ),
+      }}
+      headerTitle='Prism'
       config={config}
       data={data}
       onPublish={async (data) => {
